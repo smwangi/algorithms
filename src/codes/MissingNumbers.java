de876479e08ef31f209bfcfa62c7d8f06e231fa3
samuel wanjohi <samuel.wanjohi@un.org>
2020-06-09 21:22:22 +0300

    MergeSortedArray

---
+++ src/codes/MissingNumbers.java
@@ -0,0 +1,43 @@
+package codes;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class MissingNumbers {
+
+    public static void main(String[] args) {
+
+        int[] arr  = {11, 4, 11, 7, 13, 4, 12, 11, 10, 14};
+        int[] brr = {11, 4, 11, 7, 3, 7, 10, 13, 4, 8, 12, 11, 10, 14, 12};
+
+        System.out.println(Arrays.toString(missingNumbers(arr,brr)));
+    }
+
+    static int[] missingNumbers(int[] arr, int[] arr2){
+
+        Arrays.sort(arr);
+        Arrays.sort(arr2);
+
+        int[] a = new int[arr2.length];
+
+        int startIdx = 0;
+        for(int i = 0; i < arr.length; i++){
+
+         INNER:   for(int j = startIdx; j < arr2.length; j++){
+            System.out.println(arr[i]+" <> "+arr2[j]+" <> "+startIdx+" <> "+j);
+             startIdx++;
+                if(arr2[j] == arr[i]){
+                    a[j] =     arr2[j] - arr[i];
+
+                    break INNER;
+                }else{
+                    a[j] = arr2[j];
+                }
+
+         }
+        }
+
+        return Arrays.stream(a).filter(x -> x > 0).distinct().toArray();
+    }
+}
---
+++ src/codes/TheDuplicateNumber.java
@@ -0,0 +1,61 @@
+package codes;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+ *
+ * Example 1:
+ *
+ * Input: [1,3,4,2,2]
+ * Output: 2
+ * Example 2:
+ *
+ * Input: [3,1,3,4,2]
+ * Output: 3
+ * Note:
+ *
+ * You must not modify the array (assume the array is read only).
+ * You must use only constant, O(1) extra space.
+ * Your runtime complexity should be less than O(n2).
+ * There is only one duplicate number in the array, but it could be repeated more than once.
+ */
+public class TheDuplicateNumber {
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,2,2};
+        System.out.println(duplicateNum(arr));
+    }
+
+
+    private static int duplicateNum(int[] arr){
+        int dup = 0;
+        Set<Integer> duplicateNum = new HashSet<>();
+
+        for(int i = 0; i< arr.length;i++){
+
+
+            if(!duplicateNum.contains(arr[i])){
+                duplicateNum.add(arr[i]);
+            }else{
+                dup = arr[i];
+                break;
+            }
+        }
+        return dup;
+    }
+    /**
+     * Complexity Analysis
+     *
+     * Time complexity : \mathcal{O}(n)O(n)
+     *
+     * Set in both Python and Java rely on underlying hash tables, so insertion and lookup have amortized constant time complexities.
+     * The algorithm is therefore linear, as it consists of a for loop that performs constant work nn times.
+     *
+     * Space complexity : \mathcal{O}(n)O(n)
+     *
+     * In the worst case, the duplicate element appears twice, with one of its appearances at array index n-1n−1.
+     * In this case, seen will contain n-1n−1 distinct values, and will therefore occupy \mathcal{O}(n)O(n) space.
+     */
+}
